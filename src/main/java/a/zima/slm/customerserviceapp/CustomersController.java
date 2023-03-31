8da259482c8d6449f15254308b79a25ab85463cb
ricar <ricardo.zima.filho@gmail.com>
2023-03-31 14:06:54 +0200

    Customer class und CustomersController überarbeitet

--- src/main/java/a/zima/slm/customerserviceapp/CustomersController.java
+++ src/main/java/a/zima/slm/customerserviceapp/CustomersController.java
@@ -1,19 +1,21 @@
 package a.zima.slm.customerserviceapp;
 
+import a.zima.slm.customerserviceapp.model.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CustomersController {
     @GetMapping("/customers")
-    public List<String> getCustomer()   {
-        List<String> customers = new ArrayList<>();
-        customers.add("Anton");
-        customers.add("Berta");
-        customers.add("Cäsar");
+    public List<Customer> getCustomer() {
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("Anton", "Bauer", LocalDate.of(2000, 4, 1)));
+        customers.add(new Customer("Berta", "Suttner", LocalDate.of(1958, 6, 1)));
+        customers.add(new Customer("Julius", "Cäsar", LocalDate.of(1, 2, 4)));
         return customers;
     }
 }
